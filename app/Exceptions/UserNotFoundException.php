c0d45689efb8d80f6c58de0f7d43ae133e92881b
gui1535 <guilherme.araujo1535@gmail.com>
2023-04-22 16:01:05 -0300

    Exception usuario nao encontrado

--- app/Exceptions/UserNotFoundException.php
+++ app/Exceptions/UserNotFoundException.php
@@ -3,8 +3,16 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Support\Facades\Log;
 
 class UserNotFoundException extends Exception
 {
-    //
+
+    /**
+     * Render Exception
+     */
+    public function render()
+    {
+        return back()->withErrors(["Usuário não encontrado"])->withInput();
+    }
 }
