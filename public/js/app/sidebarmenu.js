fb5457272a876e77bd55f7aee8f84d35370e89d2
gui1535 <guilherme.araujo1535@gmail.com>
2023-04-22 16:47:50 -0300

    Correção adicionar SELECTED nos menus

--- public/js/app/sidebarmenu.js
+++ public/js/app/sidebarmenu.js
@@ -7,54 +7,55 @@ File: js
 // ============================================================== 
 // Auto select left navbar
 // ============================================================== 
-$(function() {
+$(function () {
     "use strict";
-     var url = window.location + "";
-        var path = url.replace(window.location.protocol + "//" + window.location.host + "/", "");
-        var element = $('ul#sidebarnav a').filter(function() {
-            return this.href === url || this.href === path;// || url.href.indexOf(this.href) === 0;
-        });
-        element.parentsUntil(".sidebar-nav").each(function (index)
-        {
-            if($(this).is("li") && $(this).children("a").length !== 0)
-            {
-                $(this).children("a").addClass("active");
-                $(this).parent("ul#sidebarnav").length === 0
-                    ? $(this).addClass("active")
-                    : $(this).addClass("selected");
-            }
-            else if(!$(this).is("ul") && $(this).children("a").length === 0)
-            {
-                $(this).addClass("selected");
-                
-            }
-            else if($(this).is("ul")){
-                $(this).addClass('in');
-            }
-            
-        });
+    var url = (window.location + "").split('?')[0];
+    var path = url.replace(window.location.protocol + "//" + window.location.host + "/", "");
+    const currentPath = window.location.pathname.substring(1); // Obtém a URL após o domínio
+    const element = $('ul#sidebarnav a').filter(function () {
+      const href = this.href.split('?')[0]; // Remove a parte de querystring da URL
+      const path = href.replace(window.location.origin + '/', ''); // Remove o domínio e a barra inicial da URL
+      // Verifica se a URL ou o caminho da URL contém a URL atual
+      return href === window.location.href || path === currentPath || (currentPath.startsWith(path) && path !== '');
+    });
+    element.parentsUntil(".sidebar-nav").each(function (index) {
+        if ($(this).is("li") && $(this).children("a").length !== 0) {
+            $(this).children("a").addClass("active");
+            $(this).parent("ul#sidebarnav").length === 0
+                ? $(this).addClass("active")
+                : $(this).addClass("selected");
+        }
+        else if (!$(this).is("ul") && $(this).children("a").length === 0) {
+            $(this).addClass("selected");
 
-    element.addClass("active"); 
+        }
+        else if ($(this).is("ul")) {
+            $(this).addClass('in');
+        }
+
+    });
+
+    element.addClass("active");
     $('#sidebarnav a').on('click', function (e) {
-        
-            if (!$(this).hasClass("active")) {
-                // hide any open menus and remove all other classes
-                $("ul", $(this).parents("ul:first")).removeClass("in");
-                $("a", $(this).parents("ul:first")).removeClass("active");
-                
-                // open our new menu and add the open class
-                $(this).next("ul").addClass("in");
-                $(this).addClass("active");
-                
-            }
-            else if ($(this).hasClass("active")) {
-                $(this).removeClass("active");
-                $(this).parents("ul:first").removeClass("active");
-                $(this).next("ul").removeClass("in");
-            }
+
+        if (!$(this).hasClass("active")) {
+            // hide any open menus and remove all other classes
+            $("ul", $(this).parents("ul:first")).removeClass("in");
+            $("a", $(this).parents("ul:first")).removeClass("active");
+
+            // open our new menu and add the open class
+            $(this).next("ul").addClass("in");
+            $(this).addClass("active");
+
+        }
+        else if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(this).parents("ul:first").removeClass("active");
+            $(this).next("ul").removeClass("in");
+        }
     })
     $('#sidebarnav >li >a.has-arrow').on('click', function (e) {
         e.preventDefault();
     });
-    
+
 });
