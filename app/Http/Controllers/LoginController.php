3f5cd4ddf51b803c4f236a718a7c8a66f114187b
gui1535 <guilherme.araujo1535@gmail.com>
2023-04-22 16:01:16 -0300

    Refatorando metodo de autenticação

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\ErrorUnexpectedException;
+use App\Exceptions\UserNotFoundException;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Exception;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+
+    private Request $request;
+
     public function index(Request $request)
     {
         return view('site.login', ['titulo' => 'Login']);
@@ -16,23 +22,42 @@ class LoginController extends Controller
 
     public function autenticar(LoginRequest $request)
     {
-        dd($request);
-        // Credenciais
+        try {
+            // Request
+            $this->request = $request;
+
+            // Verificando se existe usuario
+            if ($this->verificaSeExisteUsuario()) {
+                $request->session()->regenerate();
+                return redirect()->route('app.home');
+            }
+        } catch (UserNotFoundException $e) {
+            return $e->render();
+        } catch (Exception $e) {
+            return ErrorUnexpectedException::render();
+        }
+    }
+
+    private function verificaSeExisteUsuario()
+    {
         $credentials = [
-            'email' => $request->input('email'),
-            'password' => $request->input('password'),
+            'email' => $this->request->input('email'),
+            'password' => $this->request->input('password'),
         ];
-
         if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-
-            return redirect()->route('app.home');
+            return true;
+        } else {
+            throw new UserNotFoundException();
         }
     }
 
     public function sair()
     {
-        session_destroy();
-        return redirect()->route('site.index');
+        try {
+            Auth::logout();
+            return redirect()->route('site.index');
+        } catch (\Throwable $th) {
+            return ErrorUnexpectedException::render();
+        }
     }
 }
