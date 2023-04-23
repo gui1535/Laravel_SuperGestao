dce8bc55a47726ad229dd3da41eb661e43b6fac2
gui1535 <guilherme.araujo1535@gmail.com>
2023-04-22 23:21:20 -0300

    formulario para criar ou editar um cliente

--- resources/views/app/cliente/_components/form_create_edit.blade.php
+++ resources/views/app/cliente/_components/form_create_edit.blade.php
@@ -1,14 +1,45 @@
-@if(isset($cliente->id))
-    <form method="post" action="{{ route('cliente.update', ['cliente' => $cliente->id]) }}">
+@push('styles')
+    <style>
+        .required::after {
+            content: " *" !important;
+            color: red !important;
+        }
+    </style>
+@endpush
+
+@if (isset($cliente->id))
+    <form method="post" action="{{ route('cliente.update', ['cliente' => Crypt::encrypt($cliente->id)]) }}" class="row">
         @csrf
         @method('PUT')
-@else
-    <form method="post" action="{{ route('cliente.store') }}">
-        @csrf
+    @else
+        <form method="post" action="{{ route('cliente.store') }}" class="row">
+            @csrf
 @endif
-
-    <input type="text" name="nome" value="{{ $cliente->nome ?? old('nome') }}" placeholder="Nome" class="borda-preta">
-    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
-
-    <button type="submit" class="borda-preta">Cadastrar</button>
-<form>
+<div class="col-md-6 mb-3">
+    <label class="required" for="nome">Nome</label>
+    <input type="text" maxlength="60" required value="{{ $cliente->nome ?? old('nome') }}" id="nome"
+        name="nome" placeholder="Nome" class="form-control form-control-line">
+</div>
+<div class="col-md-6 mb-3">
+    <label for="email" class="required">Email</label>
+    <input type="email" maxlength="60" required value="{{ $cliente->email ?? old('email') }}" placeholder="E-mail"
+        class="form-control form-control-line" name="email" id="email">
+</div>
+<div class="form-group">
+    <label class="col-md-12">Observações</label>
+    <div class="col-md-12">
+        <textarea rows="5" maxlength="255" name="observacoes" class="form-control form-control-line">{{ $cliente->observacoes ?? old('observacoes') }}</textarea>
+    </div>
+</div>
+<div class="form-group">
+    <div class="col-sm-12 d-flex justify-content-end">
+        <button class="btn btn-primary text-white">
+            @if (isset($cliente->id))
+                Atualizar
+            @else
+                Cadastrar
+            @endif
+        </button>
+    </div>
+</div>
+</form>
