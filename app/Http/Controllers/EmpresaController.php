8f3616bce37823639fa43bcfa2cb5683723c2f9b
gui1535 <guilherme.araujo1535@gmail.com>
2023-04-23 16:54:23 -0300

    Cadastrando empresas

---
+++ app/Http/Controllers/EmpresaController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\CreateEmpresaException;
+use App\Exceptions\CreateUsuarioEmpresaException;
+use App\Exceptions\ErrorUnexpectedException;
+use App\Http\Requests\EmpresaRequest;
+use App\Models\Empresa;
+use App\Models\User;
+use Exception;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class EmpresaController extends Controller
+{
+    private Request $request;
+
+    private Empresa $empresa;
+
+    private User $usuario;
+
+    /**
+     * Blade Cadastro
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function indexCadastro(): View
+    {
+        return view('site.cadastrar', ['titulo' => 'Cadastro']);
+    }
+
+
+    public function cadastrar(EmpresaRequest $request)
+    {
+        try {
+            $this->request = $request;
+
+            $this->criaEmpresa();
+
+            $this->criaUsuarioEmpresa();
+
+            $this->criaUsuarioEmpresa();
+
+            auth()->login($this->usuario);
+
+            return redirect()->route('app.home')->with('success', 'Empresa cadastrada com suceso');
+        } catch (CreateEmpresaException $e) {
+            return $e->render();
+        } catch (CreateUsuarioEmpresaException $e) {
+            return $e->render();
+        } catch (Exception $th) {
+            return ErrorUnexpectedException::render();
+        }
+    }
+
+    /**
+     * Cria empresa com o request recebido
+     * @return void
+     */
+    private function criaEmpresa()
+    {
+        try {
+            $this->empresa = new Empresa();
+            $this->empresa->nome = $this->request->input('nome');
+            $this->empresa->cnpj = removerCaracteresEspeciais($this->request->input('cnpj'));
+            $this->empresa->telefone = removerCaracteresEspeciais($this->request->input('nome'));
+            $this->empresa->email = $this->request->input('email');
+            $this->empresa->save();
+        } catch (\Throwable $th) {
+            throw new CreateEmpresaException();
+        }
+    }
+
+    /**
+     * Cria usuario da empresa
+     * @return void
+     */
+    private function criaUsuarioEmpresa()
+    {
+        try {
+            $this->usuario = new User();
+            $this->usuario->name = "Administrador - " . $this->empresa->nome;
+            $this->usuario->email = $this->empresa->email;
+            $this->usuario->password = bcrypt($this->request->input("senha"));
+            $this->usuario->empresa_id = $this->empresa->id;
+            $this->usuario->save();
+        } catch (\Throwable $th) {
+            throw new CreateUsuarioEmpresaException();
+        }
+    }
+}
---
+++ app/Http/Requests/EmpresaRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\CnpjUniqueRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class EmpresaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nome'  => 'required|min:3|max:60',
+            'email' => 'required|email',
+            'cnpj'  => ['required','cnpj', new CnpjUniqueRule],
+            'senha'  => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'required'    => 'O campo :attribute deve ser preenchido',
+            'nome.min'    => 'O campo nome deve ter no mínimo 3 caracteres',
+            'nome.max'    => 'O campo nome deve ter no máximo 40 caracteres',
+            'email.email' => 'Insira um email válido',
+            'cnpj.cnpj'   => 'Insira um CNPJ válido',
+            'cnpj.unique' => 'O CNPJ informado já possui cadastro no sistema',
+        ];
+    }
+}
