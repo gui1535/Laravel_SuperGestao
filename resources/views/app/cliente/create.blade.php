9d2ad661ffa575318871caff5bf7a316d0d233e4
gui1535 <guilherme.araujo1535@gmail.com>
2023-04-22 23:21:07 -0300

    Blade de criar um cliente

--- resources/views/app/cliente/create.blade.php
+++ resources/views/app/cliente/create.blade.php
@@ -3,28 +3,70 @@
 @section('titulo', 'Cliente')
 
 @section('conteudo')
-    
+
     <div class="conteudo-pagina">
 
-        <div class="titulo-pagina-2">
-            <p>Adicionar Cliente</p>
-        </div>
+        <div class="page-breadcrumb">
+            <div class="row align-items-center">
+                <div class="col-9">
 
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('cliente.index') }}">Voltar</a></li>
-                <li><a href="">Consulta</a></li>
-            </ul>
+                    <div>
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb mb-0 d-flex
+                                align-items-center">
+                                <li class="breadcrumb-item">
+                                    <a href="{{ route('app.home') }}" class="link">
+                                        <i class="mdi mdi-home-outline fs-4"></i>
+                                    </a>
+                                </li>
+                                <li class="breadcrumb-item">
+                                    <a href="{{ route('cliente.index') }}" class="link">
+                                        Cliente
+                                    </a>
+                                </li>
+                                <li class="breadcrumb-item active" aria-current="page">
+                                    {{ isset($cliente) ? 'Editar' : 'Novo' }}
+                                </li>
+                            </ol>
+                        </nav>
+                        <h1 class="mb-0 fw-bold">
+                            {{ isset($cliente) ? 'Editar Cliente' : 'Novo Cliente' }}
+                        </h1>
+                    </div>
+                </div>
+            </div>
         </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                </div>
 
-        <div class="informacao-pagina">
-            <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                @component('app.cliente._components.form_create_edit')
-                @endcomponent             
+                <div class="row">
+                    <div class="col-12">
+                        @if (session('success'))
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="col-12">
+
+                    @include('app.cliente._components.form_create_edit')
+                </div>
             </div>
         </div>
 
     </div>
 
 @endsection
-
