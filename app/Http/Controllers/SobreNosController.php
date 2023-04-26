bc6bc283773eb89ac6d2af0b85772288dde5a899
gui1535 <guilherme.araujo1535@gmail.com>
2023-04-25 22:09:32 -0300

    Refatorando SobreNosController

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -6,10 +6,10 @@ use Illuminate\Http\Request;
 
 class SobreNosController extends Controller
 {
-    public function __construct() {
-        $this->middleware('log.acesso');
-    }
-    
+    /**
+     * Pagina Sobre Nos
+     * @return \Illuminate\Contracts\View\Vie
+     */
     public function sobreNos() {
         return view('site.sobre-nos');
     }
