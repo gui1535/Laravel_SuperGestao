64a9bb77274503befe0c18184c7ddb559e06b2b7
gui1535 <guilherme.araujo1535@gmail.com>
2023-04-25 22:09:38 -0300

    TesteController

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -6,11 +6,15 @@ use Illuminate\Http\Request;
 
 class TesteController extends Controller
 {
-    public function teste(int $p1, int $p2) {
+    /**
+     * METODO TESTE
+     */
+    public function teste(int $p1, int $p2)
+    {
         //echo "A soma de $p1 + $p2 é: ".($p1+$p2);
-        
+
         //return view('site.teste', ['p1' => $p1, 'p2' => $p2]); //array associativo
-        
+
         //return view('site.teste', compact('p1', 'p2')); //compact
 
         return view('site.teste')->with('p1', $p1)->with('p2', $p2); //with()
